cde78f27129198bf107684d62fdafebd101b90f0
Aniket <iamaniketkumarmaner@gmail.com>
2026-02-04 17:30:47 +0530

    Delete src/interfaces/ISocialRecovery.sol

--- src/interfaces/ISocialRecovery.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-interface ISocialRecovery {
-    function reportCompromised(uint256 tokenId) external;
-    function initiateBackupUpdate(uint256 tokenId, address newBackup) external;
-    function finalizeBackupUpdate(uint256 tokenId) external;
-    function recoverIdentity(uint256 tokenId) external;
-}
