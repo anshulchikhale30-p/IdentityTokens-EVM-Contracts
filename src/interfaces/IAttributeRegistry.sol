3c0eb6946884d81fff7a5644497f53903b6903e0
Aniket <iamaniketkumarmaner@gmail.com>
2026-02-04 17:30:07 +0530

    Delete src/interfaces/IAttributeRegistry.sol

--- src/interfaces/IAttributeRegistry.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-interface IAttributeRegistry {
-    function setAttribute(uint256 tokenId, bytes32 keyHash, bytes calldata value) external;
-    function getAttribute(uint256 tokenId, bytes32 keyHash) external view returns (bytes memory);
-}
