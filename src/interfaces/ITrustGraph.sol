c776c80568826dc5b39f46aef5bc0e44e6012402
Aniket <iamaniketkumarmaner@gmail.com>
2026-02-04 17:30:35 +0530

    Delete src/interfaces/ITrustGraph.sol

--- src/interfaces/ITrustGraph.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-import {DataTypes} from "../libraries/DataTypes.sol";
-
-interface ITrustGraph {
-    function endorse(uint256 fromId, uint256 toId, string calldata connType, uint256 ttl) external;
-    function revokeEndorsement(uint256 fromId, uint256 toId, uint256 index) external;
-    function getEndorsements(uint256 tokenId) external view returns (DataTypes.Endorsement[] memory);
-}
