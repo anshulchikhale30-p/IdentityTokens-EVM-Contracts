b4066ab219c715fe1a0e9f5bc49df502ae9c0b5d
anshulchikhale30p <anshulchikhale30p@gmail.com>
2026-02-07 16:39:36 +0530

    Update MiniDIT.sol

--- src/MiniDIT.sol
+++ src/MiniDIT.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
-import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
 
 /**
  * @title MiniDIT
@@ -19,19 +19,13 @@ contract MiniDIT is ERC721 {
         string ageGroup; // e.g. "18+", "21+", etc.
         bool compromised;
 
-       function _beforeTokenTransfer(
-    address from,
-    address to,
-    uint256 tokenId,
-    uint256 batchSize
-) internal override {
+function _update(address to, uint256 tokenId, address auth) internal override returns (address) {
+    address from = _ownerOf(tokenId);
     if (from != address(0) && to != address(0)) {
         revert("MiniDIT: identity tokens are non-transferable");
     }
-    super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    return super._update(to, tokenId, auth);
 }
-
-    }
 
     // tokenId => metadata
     mapping(uint256 => IdentityMetadata) public identities;
